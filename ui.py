056ce35fcdd0bee5b009ca9d66c554281caae8f2
Nathaniel Daley <ndaley64@gmail.com>
2025-03-08 22:05:50 -0600

    quiz interface gets first question from quiz_brain

--- ui.py
+++ ui.py
@@ -1,11 +1,14 @@
 import tkinter
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 QUESTION_FONT = ("Arial", 20, "italic")
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, background=THEME_COLOR)
@@ -14,7 +17,7 @@ class QuizInterface:
         self.score_text.grid(column=1, row=0)
 
         self.question_canvas = Canvas(width=300, height=250, highlightthickness=0, background="white")
-        self.question_canvas_text = self.question_canvas.create_text(150, 125, text="test",
+        self.question_canvas_text = self.question_canvas.create_text(150, 125, width=280, text="test",
                                                                      font=QUESTION_FONT, fill=THEME_COLOR)
         self.question_canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
@@ -26,6 +29,10 @@ class QuizInterface:
         self.false_button = Button(image=false_button_image, highlightthickness=0)
         self.false_button.grid(column=1, row=2)
 
-
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        question_text = self.quiz.next_question()
+        self.question_canvas.itemconfigure(self.question_canvas_text, text=question_text)
